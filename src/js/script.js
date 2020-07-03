4d1d8276ba400a52b4ed410dce712fdb11af5fcb
Aleksandr <shoppingplaneta@gmail.com>
2020-07-03 14:48:52 +0300

    Сделан слайдер в отзывах

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    dots: false, //точеки внизу слайдера
+    speed: 1200,
+    adaptiveHeight: false, //высота слайда будет одинаковая, в независимости от высоты контента
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/reviews/prev-icon.png" ,alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/reviews/next-icon.png" ,alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+        dots: true,
+        arrows: false,
+        }
+      }
+    ]
+  });
+});
